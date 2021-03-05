e1e484ec3b761c1d47ac4db5b8464f6d5d65b590
kml160030 <kalyb369@hotmail.com>
2021-03-05 11:50:22 -0600

    added getting a movie by id, name and getting all movies by their year

--- src/main/java/com/revature/web/controllers/MovieController.java
+++ src/main/java/com/revature/web/controllers/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,5 +26,21 @@ public class MovieController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Movie> getAllMovies(){
         return movieService.getAllMovies();
+    }
+
+
+    @GetMapping(path = "/id", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Movie getMovieById(@RequestParam int id){
+        return movieService.getMovieById(id);
+    }
+
+    @GetMapping(path = "/name", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Movie getMovieByName(@RequestParam String name){
+        return movieService.getMovieByName(name);
+    }
+
+    @GetMapping(path = "/year", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Movie> getMoviesByYear(@RequestParam int year){
+        return movieService.getMoviesByYear(year);
     }
 }
