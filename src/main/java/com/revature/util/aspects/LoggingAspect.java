2f7c1509378cb52bfaf93a3f5f7fdc8ff6d7add6
Cole Space <cospace@unm.edu>
2021-03-08 10:51:41 -0700

    added the logic to the logging aspect to log before a method is called, and to log any return values or exceptions thrown by the method

--- src/main/java/com/revature/util/aspects/LoggingAspect.java
+++ src/main/java/com/revature/util/aspects/LoggingAspect.java
@@ -1,4 +1,43 @@
 package com.revature.util.aspects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    private final Logger logger = LogManager.getLogger(LoggingAspect.class);
+
+    @Pointcut("within(com.revature..*)")
+    public void logAllPointcut(){}
+
+    @Before("logAllPointcut()")
+    public void logMethodStart(JoinPoint jp){
+        String methodSig = extractMethodSignature(jp);
+        String argStr = Arrays.toString(jp.getArgs());
+        logger.info("{} invoked at {}; input arguments: {}", methodSig, LocalDateTime.now(), argStr);
+    }
+
+    @AfterReturning(pointcut = "logAllPointcut()", returning = "returned")
+    public void logMethodReturn(JoinPoint jp, Object returned){
+        String methodSig = extractMethodSignature(jp);
+        logger.info("{} successfully returned at {} with a value of {}", methodSig, LocalDateTime.now(), returned);
+    }
+
+    @AfterThrowing(pointcut = "logAllPointcut()", throwing = "e")
+    public void logErrorOccurring(JoinPoint jp, Exception e){
+        String methodSig = extractMethodSignature(jp);
+        logger.error("{} was thrown in method {} at {} with message: {}", e.getClass().getSimpleName(), methodSig, LocalDateTime.now(), e.getMessage());
+    }
+
+    private String extractMethodSignature(JoinPoint jp) {
+        return jp.getTarget().getClass().toString() + "." + jp.getSignature().getName();
+    }
 }
