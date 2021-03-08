b6dab8f659814156750d276c352e091c1885c683
Cole Space <cospace@unm.edu>
2021-03-07 17:26:29 -0700

    Created the intercom package to communicate to the OMDb API, as well as now allow for users to add a movie to our database that is in the OMDb API database

--- src/main/java/com/revature/services/MovieService.java
+++ src/main/java/com/revature/services/MovieService.java
@@ -4,20 +4,37 @@ import com.revature.entities.Movie;
 import com.revature.exceptions.InvalidRequestException;
 import com.revature.exceptions.ResourceNotFoundException;
 import com.revature.repos.MovieRepository;
+import com.revature.web.intercom.OMDbClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
 public class MovieService {
 
     private MovieRepository movieRepo;
+    private OMDbClient omdb;
 
     @Autowired
-    public MovieService(MovieRepository movieRepo) {
+    public MovieService(MovieRepository movieRepo, OMDbClient omdb) {
         this.movieRepo = movieRepo;
+        this.omdb = omdb;
+    }
+
+    public void save(Movie m){
+        movieRepo.save(m);
+    }
+
+    public boolean saveNewMovie(Movie m){
+        Optional<Movie> movie = movieRepo.findMovieByName(m.getName());
+        if(movie.isPresent()){
+            return false;
+        }
+        movieRepo.save(omdb.getMovieInformation(m.getName()));
+        return true;
     }
 
     public List<Movie> getAllMovies(){
@@ -37,5 +54,13 @@ public class MovieService {
 
     public List<Movie> getMoviesByYear(int year){
         return movieRepo.findMoviesByYear(year);
+    }
+
+    public List<Movie> getMoviesByGenre(String genre){
+        return movieRepo.findMoviesByGenre(genre);
+    }
+
+    public List<Movie> getMoviesByDirector(String director){
+        return movieRepo.findMoviesByDirector(director);
     }
 }
