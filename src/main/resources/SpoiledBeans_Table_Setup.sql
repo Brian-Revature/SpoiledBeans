234a3b6bd9fdda616ebf824318c0a0d96639d501
kml160030 <kalyb369@hotmail.com>
2021-03-08 10:49:35 -0600

    changed database setup script to have text for synopsis instead of varchar

--- src/main/resources/SpoiledBeans_Table_Setup.sql
+++ src/main/resources/SpoiledBeans_Table_Setup.sql
@@ -64,13 +64,15 @@ CREATE TABLE movies (
 	name		varchar(180),
 	director	varchar(51),
 	genre		varchar(25),
-	synopsis	varchar(25),
+	synopsis	text,
 	release_date	int not null,
 	
 	CONSTRAINT movie_id
 	PRIMARY KEY (id)
 
 );
+
+
 
 --CREATE TABLE review (
 --
@@ -133,7 +135,12 @@ CREATE TABLE favorites (
 
 --insert into movies ("name",release_date)
 --	values('dummyMovieTheSequel',2003);
+--insert into movies ("name",director,genre,synopsis,release_date)
+--	values('dummy the Third', 'Kalyb Levesque', 'comedy', 'a funny conclusion ', 2005);
 
+
+--select * from movies;
+--select * from users;
 -- these drops should be in order to delete everything for recreation
 
 --drop table user_reviews;
