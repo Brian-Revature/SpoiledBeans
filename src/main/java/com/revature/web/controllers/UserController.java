7f0adbd8c3a80a46b8e6f9f4994c4836227e0309
Gerts19 <gregory.gertsen@revature.net>
2021-03-12 11:58:08 -0800

    Added Javadocs to the UserController

--- src/main/java/com/revature/web/controllers/UserController.java
+++ src/main/java/com/revature/web/controllers/UserController.java
@@ -18,6 +18,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * An endpoint that controls interactions with user and a user's list of favorite movies
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -32,52 +35,92 @@ public class UserController {
         this.authService = authService;
     }
 
+    /**
+     * Returns the user that is currently logged based on user id obtained from JWT
+     * @param request
+     * @return
+     */
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserById(HttpServletRequest request) {
         return userService.getUserById(authService.getUserId(getToken(request)));
     }
 
+    /**
+     * Returns the user that matches the supplied username
+     * @param username
+     * @return
+     */
+    //TODO Change the parameter to a @requestBody and pass it a UserDTO
     @GetMapping(path = "/getuserbyusername", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserByUsername(@RequestParam String username) {
         return userService.getUserByUsername(username);
     }
 
-
+    /**
+     * Updates any values that are present in the UserDTO
+     * @param userdto
+     * @param request
+     */
     @PutMapping(path = "/update", consumes =  MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public void updateUser(@RequestBody UserDTO userdto, HttpServletRequest request) {
         userService.updateUser(userdto, authService.getUserId(getToken(request)));
     }
 
     //--------------------- Favorites -------------------------------
+
+    /**
+     * Returns a list of the currently signed in user's favorite movies
+     * @param request
+     * @return
+     */
     @GetMapping(path= "/myfavorites",produces= MediaType.APPLICATION_JSON_VALUE)
     public FavoritesDTO getUserFavorites(HttpServletRequest request) {
-        //Not clear how we are getting the user id or user data at this point
         return userService.getUserFavorites(authService.getUserId(getToken(request)));
     }
 
+    /**
+     * Returns a list of a user's favorite movies based on the supplied username
+     * @param userdto
+     * @return
+     */
     @GetMapping(path= "/userfavorites",produces= MediaType.APPLICATION_JSON_VALUE)
     public FavoritesDTO getUserFavorites(@RequestBody UserDTO userdto) {
         return userService.getUserFavorites(userdto.getUsername());
     }
 
+    /**
+     * Adds a favorite movies to the currently logged in user based on user id obtained from JWT
+     * @param moviesdto
+     * @param request
+     */
     @PostMapping(path="/addfavorite",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addFavoriteMovie(@RequestBody final MoviesDTO moviesdto, HttpServletRequest request) {
         userService.addFavorite(moviesdto, authService.getUserId(getToken(request)));
     }
 
+    /**
+     * Deletes a movie from the currently logged in user's favorites list
+     * @param moviesDTO
+     * @param request
+     */
     @DeleteMapping(path = "/deletefavorite",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
     public void deleteUserFavorite(@RequestBody final MoviesDTO moviesDTO, HttpServletRequest request) {
         userService.deleteUserFavorite(moviesDTO, authService.getUserId(getToken(request)));
     }
 
+    /**
+     * Returns a user's favorite movies list ordered by movie name
+     * @param ascending if true, list is ordered ascending. If false, list is ordered descending
+     * @param request
+     * @return
+     */
     @GetMapping(path= "/favoritesbyname",produces= MediaType.APPLICATION_JSON_VALUE)
     public List<Movie> getUserFavoritesByName(@RequestParam boolean ascending, HttpServletRequest request) {
-        //Not clear how we are getting the user id or user data at this point
         return userService.getUserFavoritesByName(ascending, authService.getUserId(getToken(request)));
     }
 
     //----------------------------------------------------------------------------
-
+    // Helper method to process a user's cookies
     private String getToken(HttpServletRequest request){
         Cookie[] reqCookies = request.getCookies();
 
