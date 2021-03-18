f477d6d23d785474c9292ca2f09a9e5b0e6e522d
chris <armoredmoose@gmail.com>
2021-03-18 10:27:07 -0500

    javadocs for repos

--- src/main/java/com/revature/repos/MovieRepository.java
+++ src/main/java/com/revature/repos/MovieRepository.java
@@ -1,25 +1,27 @@
 package com.revature.repos;
 
 import com.revature.entities.Movie;
-import com.revature.entities.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
+/**
+ * Repository to handle sending and retrieving movie data between client and database.
+ */
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
 
 
-    Optional<Movie> findMovieByName(String name);
-    List<Movie> findMoviesByYear(int year);
-    List<Movie> findMoviesByGenre(String genre);
-    List<Movie> findMoviesByDirector(String director);
+    Optional<Movie> findMovieByName(final String name);
+    List<Movie> findMoviesByYear(final int year);
+    List<Movie> findMoviesByGenre(final String genre);
+    List<Movie> findMoviesByDirector(final String director);
 
+    //Get the average rating for a given movie by movie name
     @Query(value = "SELECT AVG(r.rating) FROM review r JOIN movie_review mr ON mr.review_id = r.id JOIN movies m ON m.id = mr.movie_id WHERE m.name = ?1", nativeQuery = true)
-    double findMovieRating(String name);
+    double findMovieRating(final String name);
 
 }
