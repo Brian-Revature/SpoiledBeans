1dbd9088b6406ab2fc577dfd72620826caf0c987
chris <armoredmoose@gmail.com>
2021-03-18 14:18:40 -0500

    added javadocs

--- src/main/java/com/revature/services/MovieService.java
+++ src/main/java/com/revature/services/MovieService.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Service CLass which handles getting Movie data from database or sending Movie data to database.
+ *  Service Class which
  */
 @Service
 public class MovieService {
